103d8d5338802000f247602b5631fc45294ab6a0
shunto-ishiguro <b1024017@fun.ac.jp>
2025-08-31 15:31:49 +0900

    BannerSlider作成

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Header from "../components/Header";
+import BannerSlider from "../components/BannerSlider";
 
 export default function About() {
     return (
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <Header />
+            <BannerSlider />
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+
                 <Image
                     className="dark:invert"
                     src="/next.svg"
