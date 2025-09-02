9cd9fd1314faecaba1c53c879608f371281f8c26
shunto-ishiguro <b1024017@fun.ac.jp>
2025-09-02 15:58:12 +0900

    Plannerを改良した

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,7 +1,10 @@
 import NodeArea from "./NodeArea";
+import Planner from "./Planner";
 
 export default function Demo() {
     return (
-        <NodeArea />
+        <main className="min-h-screen p-8 bg-gray-50">
+            <Planner />
+        </main>
     );
 } 
