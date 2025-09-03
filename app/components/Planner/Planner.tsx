ff2a7ddcbcedeca56121f11c5da3325c120b00d6
bancho <riceomeletomuraisu@gmail.com>
2025-09-04 01:05:01 +0900

    page,planner,plannercontent,plannersidebarに説明コメントを付けた

--- app/components/Planner/Planner.tsx
+++ app/components/Planner/Planner.tsx
@@ -2,12 +2,13 @@
 
 "use client";
 import { useRef } from "react";
-import { useReactToPrint } from "react-to-print";
+import { useReactToPrint } from "react-to-print"; //PDF出力するライブラリ
 import { usePlanner } from "./usePlanner";
 import PlannerSidebar from "./PlannerSidebar";
 import PlannerContent from "./PlannerContent";
 
 //プラン作成画面全体のコンポーネント。プラン作成のすべてを管理、表示している。
+//PlannerSidebarは上の操作パネルのところ。PlannerContentは下のプラン内容のところ。
 export default function Planner() {
     const planner = usePlanner();
     const printRef = useRef<HTMLDivElement>(null);
